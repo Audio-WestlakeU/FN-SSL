ce6da8a5cea0ef4461ee85f859b17adaaca57f3f
bbox <39715642+wangyabo123@users.noreply.github.com>
2024-10-22 15:16:12 +0800

    Update Simu.py

--- IPDnet/Simu.py
+++ IPDnet/Simu.py
@@ -56,8 +56,8 @@ dataset = at_dataset.RandomTrajectoryDataset(
 	sourceDataset = sourceDataset,
 	num_source = Parameter(args.sources, discrete=True), # Random number of sources from list-args.sources
 	source_state = args.source_state,
-	room_sz = Parameter([6,6,2.5], [10,8,6]),  	# Random room sizes from 3x3x2.5 to 10x8x6 meters
-	T60 = rt_range,					# Random reverberation times from 0.2 to 1.3 seconds
+	room_sz = Parameter([6,6,2.5], [10,8,6]),  	# Random room sizes from 6x6x2.5 to 10x8x6 meters
+	T60 = rt_range,					# Random reverberation times
 	abs_weights = Parameter([0.5]*6, [1.0]*6),  # Random absorption weights ratios between walls
 	array_setup = array_setup,
 	array_pos = Parameter([0.35,0.35,0.3], [0.65,0.65,0.5]), # Ensure a minimum separation between the array and the walls
